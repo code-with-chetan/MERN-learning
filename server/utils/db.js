d24d68e2d641ecd1ac7fb52eb989ae91102b6882
chetan rana <ranachetan455@gmail.com>
2025-10-26 09:57:20 +0545

    Securing the private data

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-const URI = "mongodb://127.0.0.1:27017/mern_admin";
-// mongoose.connect(URI);
+const URI = process.env.MONGODB_URI;
 
 const connectionDB = async () => {
   try {
