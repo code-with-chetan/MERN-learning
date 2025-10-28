f7c192efd5428ac7b40bf9e62a66889b770a4912
chetan rana <ranachetan455@gmail.com>
2025-10-28 11:00:52 +0545

    react page navigation with react router dom

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Services } from "./pages/Services";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+
 const App = () => {
-  return <h1>Hello root page</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
---
+++ clients/src/pages/Services.jsx
@@ -0,0 +1,3 @@
+export const Services = () => {
+  return <h1>Services page</h1>;
+};
