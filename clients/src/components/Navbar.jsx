0098fbe8bca933c09dedd047e1b5d96bbd4f3a32
chetan rana <ranachetan455@gmail.com>
2025-10-28 12:41:33 +0545

    Navbar in reactjs

---
+++ clients/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return (
+    <>
+      <header>
+        <div className="container">
+          <div className="logo-brand">
+            <NavLink to="/">LearnGain</NavLink>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/services">Services</NavLink>
+              </li>
+              <li>
+                <NavLink to="/register">Register</NavLink>
+              </li>
+              <li>
+                <NavLink to="/login">login</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
